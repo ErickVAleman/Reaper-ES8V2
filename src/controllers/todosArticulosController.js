b5ca7d376e1e14509dc993e607d047420e1283ee
ErickVAleman <Erickva70@gmail.com>
2018-09-07 10:54:57 -0500

    update

--- src/controllers/todosArticulosController.js
+++ src/controllers/todosArticulosController.js
@@ -2,7 +2,6 @@ import db from '../db'
 
 // Esto es un Ejemplo de como usarlo
 async function todosArticulosController (req, res){
-  articulosModel()
   let query = `SELECT Articulo, Nombre, UnidadCompra, UnidadVenta FROM Articulos`;
   let database = new db('bo',query);
   let resultado = await database.rawQuery();
